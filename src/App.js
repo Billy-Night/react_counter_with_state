8df6cedcdfae6a95c6be3a647bdba8e164eb4216
Billy-Night <billy.m.nightingale@gmail.com>
2022-03-31 12:47:36 +0200

    built a working counter using state, there is one added component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React, { useState } from "react";
+
+import Header from "./components/Header";
+import "./App.css";
 
 function App() {
+  let [name] = useState("Marc");
+  let [counter, setCounter] = useState(0);
+
+  const increment = () => setCounter(counter + 1);
+  const decrease = () => setCounter(counter - 1);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>State testing</h1>
+      <Header subtitle="Amazing app" />
+      <h3>HEY! {name} how are you?</h3>
+      <hr />
+      <h1>Counter value: {counter} </h1>
+      <button onClick={increment}>+</button>
+      <button onClick={decrease}>-</button>
     </div>
   );
 }
